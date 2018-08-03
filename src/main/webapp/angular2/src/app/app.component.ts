3e096d7d0a04b45059195ebb2bdbb850215b0ea4
RRRLuxoft <rrr.lands@gmail.com>
2018-08-03 21:38:57 +0200

    2. Build Mobile Responsive UI // Create available rooms table on UI

--- src/main/webapp/angular2/src/app/app.component.ts
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -9,16 +9,22 @@ import {FormControl, FormGroup} from "@angular/forms";
 export class AppComponent implements OnInit{
   public submitted: boolean;
   roomsearch : FormGroup;
+  rooms: Room[];
 
   ngOnInit() {
     this.roomsearch = new FormGroup({
       checkin: new FormControl(''),
       checkout: new FormControl('')
     });
+    this.rooms=ROOM;
   }
 
   onSubmit({value, valid}: {value:Roomsearch, valid:boolean}) {
     console.log(value);
+  }
+
+  reserveRoom(value:string) {
+    console.log("Room id for reservation: " + value);
   }
 
 }
@@ -27,3 +33,32 @@ export interface Roomsearch {
   checkin: string;
   checkout: string;
 }
+
+
+export interface Room {
+  id: string;
+  roomNumber: string;
+  price: string;
+  links: string;
+}
+
+var ROOM:Room[] = [
+  {
+    "id" : "321741",
+    "roomNumber" : "107",
+    "price" : "35",
+    "links" : ""
+  },
+  {
+    "id" : "998855663322554",
+    "roomNumber" : "703",
+    "price" : "205",
+    "links" : ""
+  },
+  {
+    "id" : "0000321741",
+    "roomNumber" : "1071",
+    "price" : "350",
+    "links" : ""
+  }
+];
