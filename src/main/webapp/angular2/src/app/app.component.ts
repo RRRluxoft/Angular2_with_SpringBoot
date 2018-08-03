ec9c2f7489f220886ad68b740e2ba2314130bb89
RRRLuxoft <rrr.lands@gmail.com>
2018-08-03 21:12:05 +0200

    2. Build Mobile Responsive UI // Create and style an Angular2 component

---
+++ src/main/webapp/angular2/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  public submitted: boolean;
+  roomsearch : FormGroup;
+
+  ngOnInit() {
+    this.roomsearch = new FormGroup({
+      checkin: new FormControl(''),
+      checkout: new FormControl('')
+    });
+  }
+
+  onSubmit({value, valid}: {value:Roomsearch, valid:boolean}) {
+    console.log(value);
+  }
+
+}
+
+export interface Roomsearch {
+  checkin: string;
+  checkout: string;
+}
